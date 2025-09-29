782ea0eaa3b141ef95b73e8038f65f870e2317cc
aaron silverstein <aaronsilverstein@MacBookPro.lan>
2025-09-29 17:09:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// // Run when the page fully loads
+//   window.addEventListener('load', () => {
+//     // Scroll instantly to the top
+//     window.scrollTo(0, 0);
+
+//     // Optional: for smooth scrolling instead of instant jump
+//     // window.scrollTo({ top: 0, behavior: 'smooth' });
+//   });
